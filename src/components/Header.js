9fe49780678f796f075540845d8890908265b2cc
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-03 08:51:00 -0800

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="jumbotron jumbotron-fluid">
+            <h1>Employee Directory</h1>
+            <p>sort through a list of employees</p>
+        </header>
+    )
+}
+
+export default Header;
